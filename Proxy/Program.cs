e55d98298423da625aabf04470893edf8267feef
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2023-10-25 23:36:31 +0200

    Docker containerisation setup.

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,21 +1,13 @@
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 // Add services to the container.
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
-
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "LoggingAPI V1");
-    });
 
 app.UseHttpsRedirection();
 app.MapReverseProxy();
